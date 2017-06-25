7f99e9e5b656d3534ef65175198200e2c03aa5db
MathieuFan <mathieuquintois@gmail.com>
2017-06-26 01:41:32 +0200

    Update sound.js

--- sound.js
+++ sound.js
@@ -13,4 +13,42 @@ bot.on('message', msg => {
         msg.channel.send("yolo test effectué").catch(console.error)
         console.log('commande effectuée')
     }
+    if (!msg.guild) return;
+  if (msg.content.startsWith(prefix+'join')) {
+    if (msg.member.voiceChannel) {
+      msg.member.voiceChannel.join()
+        .then(connection => { 
+          msg.reply('J\'ai rejoins le serveur comme prévu !');
+        })
+        console.log("soundbox joined the server")
+    } else {
+      msg.reply('Tu as besoin d\'être connecté a un salon vocal pour me faire venir');
+    }
+  }
+  
+  
+   if (!msg.guild) return;
+  if (msg.content.startsWith(prefix+'leave')) {
+    if (msg.member.voiceChannel) {
+      msg.member.voiceChannel.leave()
+      msg.channel.send('Je quitte le salon vocal.')
+      console.log("soundbox leave the server")
+    } else {
+      msg.reply('Tu as besoin d\'être connecté a un salon vocal pour me faire partir');
+    }
+  }
+  
+   if (!msg.guild) return;
+   if (msg.content.startsWith(prefix+"souffrance")) {
+   const voiceChannel = msg.member.voiceChannel;
+voiceChannel.join()
+      .then(connection => {
+        msg.channel.send("Je suis laaa")
+        console.log("A joué le son Anti souffrance") 
+        const dispatcher = connection.playFile('./souffrance.mp3');
+dispatcher.setVolume(2);
+dispatcher.on('end', () => voiceChannel.leave());
+}).catch(console.error)
+   
+}
 })
